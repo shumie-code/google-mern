b4501b3c668dda1fb788152508835b118aa893c7
Petros Shumie <shumie.peter@gmail.com>
2020-01-27 18:53:50 -0500

    Created CRUD  functionality within bookController for handling incoming API data from google API.

---
+++ controllers/googleController.js
@@ -0,0 +1,12 @@
+const db = require("../models");
+
+// Defining methods for the bookController
+module.exports = {
+    findAll: function(req, res) {
+        db.Book.find(req.query) 
+            .then(dbBook => res.json(dbBook))
+            .catch(err => res.status(422).json(err));
+        },
+        findById: 
+    }
+}

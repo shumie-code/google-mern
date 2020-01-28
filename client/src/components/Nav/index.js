2a5d53dad134d04de84dd392cf7cef62d3a251fc
Petros Shumie <shumie.peter@gmail.com>
2020-01-28 11:19:54 -0500

    Created render methods using conditional logic to display appropriate page link when a page is either saved or a bok is searched.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -29,4 +29,49 @@ class Nav extends Component {
     componentWillUnmount() {
         window.removeEventListener("resize", this.updateWidth);
     }
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
+                <Link className="navbar-brand" to="/">
+                    Google Books
+                </Link>
+                <button
+                onClick={this.toggleNav}
+                className="navbar-toggler"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                >
+                    <apn className="navbar-toggler-icon" />
+                </button>
+                <div className={`${this.state.open ? "" : "collapse "}navbar-collapse`}id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link 
+                            onClick={this.toggleNav}
+                            className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}
+                            to="/"
+                            >
+                                Search
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link 
+                            onClick={this.toggleNav}
+                            className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
+                            to="/saved"
+                            >
+                                Saved
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
 }
+
+export default Nav;

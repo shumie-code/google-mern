735589196c28d0e511498525a5a174a5bc5b1925
Petros Shumie <shumie.peter@gmail.com>
2020-01-28 11:10:24 -0500

    Added methods to handle window resizing and eventlistenrs to update the corrosponding width of the navbar as the window changes size.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,5 +7,26 @@ class Nav extends Component {
         open: false,
         width: window.innerWidth
     };
-    
+
+    updateWidth = () => {
+        const newState = { width: window.innerWidth };
+
+        if (this.state.open && newState.width > 991) {
+            newState.open = false;
+        }
+
+        this.setState(newState);
+    };
+
+    toggleNav = () => {
+        this.setState({ open: !this.state.open });
+    };
+
+    componentDidMount() {
+        window.addEventListener("resize", this.updateWidth);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateWidth);
+    }
 }

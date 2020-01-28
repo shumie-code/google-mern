c248276c4c3d177e0f5a6f01d8d39995d0d00613
Petros Shumie <shumie.peter@gmail.com>
2020-01-28 11:46:43 -0500

    Created footer for application.

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <hr />
+            <p className="pull-right">
+                <i className="fab fa-github" /> Rendered with React.js
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;

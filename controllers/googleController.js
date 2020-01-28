e0b6693dbb01e311a4ee49b61ef6d774e141c02c
Petros Shumie <shumie.peter@gmail.com>
2020-01-28 10:31:41 -0500

    Created googl controller file to handle incoming JSON response data coming in from google book api. The api data then is filtered and the appropriate contents are grabbed. Then the data is stored into the book database if any errors arise an error code of 422 will occur on the server-side.

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -1,12 +1,36 @@
+const axios = require("axios");
 const db = require("../models");
 
-// Defining methods for the bookController
+// googleController will use axios for get methods on the google api we are trying to return json data from
+// It will filter and find all the json data we are looking for as defined in our database schema
+// Then it wil store the json data in the book database
+
 module.exports = {
-    findAll: function(req, res) {
-        db.Book.find(req.query) 
-            .then(dbBook => res.json(dbBook))
-            .catch(err => res.status(422).json(err));
-        },
-        findById: 
-    }
-}
+  findAll: function(req, res) {
+    const { query: param } = req;
+    axios
+      .get("https://www.googleapis.com/books/v1/volumes", {
+        params
+      })
+      .then(results =>
+        results.data.items.filter(
+          result =>
+            result.volumeInfo.title &&
+            result.volumeInfo.infoLink &&
+            result.volumeInfo.authors &&
+            result.volumeInfo.description &&
+            result.volumeInfo.imageLinks &&
+            result.volumeInfo.imageLinks.thumbnail
+        )
+      )
+      .then(apiBooks =>
+        db.Book.find().then(dbBooks =>
+          apiBooks.filter(apiBook =>
+            db.Books.every(dbBook => dbBook.googleId.toString() !== apiBook.id)
+          )
+        )
+      )
+      .then(books => res.json(books))
+      .catch(err => res.status(422).json(err));
+  }
+};

fac19e3b7ed866996c4e9e70c868fc9ea4f1ea58
Petros Shumie <shumie.peter@gmail.com>
2020-01-28 10:37:43 -0500

    Added imports to pass down properties from Saved parent component. Created Saved parent component that extends and lends properties down to functional components for rendering saved api book datafrom the google books api.

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import Card from "../components/Card";
+import Book from "../component/Book";
+import Footer from "../components/Footer";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import { List } from "../components/List";
+
+class Saved extends Component {
+    state = {
+        books: []
+    };
+
+    
+}

a77e48854f360ee5535cad535973669ccf0ed2be
Petros Shumie <shumie.peter@gmail.com>
2020-01-28 10:55:26 -0500

    Created render methods for each of saving and deletig books. The methods are rendered by two different onCLick functions that act to either add json data to the database or delete it. A messsage is redered for saving and deleting data from the database.

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -8,25 +8,78 @@ import { Col, Row, Container } from "../components/Grid";
 import { List } from "../components/List";
 
 class Saved extends Component {
-    state = {
-        books: []
-    };
+  state = {
+    books: []
+  };
 
-    componentDidMount() {
-        this.getSavedBooks();
-    }
+  componentDidMount() {
+    this.getSavedBooks();
+  }
 
-    getSavedBooks = () => {
-        API.getSavedBooks()
-        .then(res => 
-            this.setState({
-                books: res.data
-            })
-            )
-            .catch(err  => console.log(err));
-    };
+  getSavedBooks = () => {
+    API.getSavedBooks()
+      .then(res =>
+        this.setState({
+          books: res.data
+        })
+      )
+      .catch(err => console.log(err));
+  };
 
-    handleBookDelete = id => {
-        API.deleteBook(id).then(res => this.getSavedBooks());
-    };
+  handleBookDelete = id => {
+    API.deleteBook(id).then(res => this.getSavedBooks());
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1 className="text-center">
+                <strong>(MERN) Book Search</strong>
+              </h1>
+              <h2 className="text-center">
+                Find and save good reads to a reading list!
+              </h2>
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+            <Card title="Saved Books" icon="download">
+              {this.state.books.length ? (
+                <List>
+                  {this.state.books.map(book => (
+                    <Book
+                      key={book._id}
+                      title={book.title}
+                      subtitle={book.subtitle}
+                      link={book.link}
+                      authors={book.authors.join(", ")}
+                      description={book.description}
+                      image={book.image}
+                      Button={() => (
+                        <button
+                          onClick={() => this.handle.BookDelete(book._id)}
+                          className="btn btn-danger ml-2"
+                        >
+                          Delete
+                        </button>
+                      )}
+                    />
+                  ))}
+                </List>
+              ) : (
+                <h2 className="text-center">Your reading list is empty</h2>
+              )}
+            </Card>
+          </Col>
+        </Row>
+        <Footer />
+      </Container>
+    );
+  }
 }
+
+export default Saved;

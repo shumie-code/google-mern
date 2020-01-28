0c9344db701fc80bb37797c7c83edd511923fe13
Petros Shumie <shumie.peter@gmail.com>
2020-01-28 11:06:36 -0500

    Imported the style.css file to the navbar for bootstrap styling. Created a parent component that extends its class to the size of the navbar component.

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+class Nav extends Component {
+    state = {
+        open: false,
+        width: window.innerWidth
+    };
+    
+}

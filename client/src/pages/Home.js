e6d1b1a7e94c54a6606cc63ffb1617de0c5c11dc
Petros Shumie <shumie.peter@gmail.com>
2020-01-27 19:27:45 -0500

    Created a HomePage for the user to add input request queries to the database after calling to the google api, the Home page will have a home component that extends its state to be changed as the user inputs different queries. This state will be passed via props to the child components of the Home component for rendering on the page.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Jumbotron from "../components/Jumbotron";
+import Card from "../components/Card";
+import Form from "../components/Form";
+import Book from "../components/Book";
+import Footer from "../components/Footer";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import { List } from "../components/List";
+
+class Home extends Component {
+    state = {
+        books: [],
+        q: "",
+        message: "Look for a book!"
+    };
+
+    
+}

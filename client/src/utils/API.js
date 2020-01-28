afe6ba0e482c12c2b349e08a72f2415a7ad15cdc
Petros Shumie <shumie.peter@gmail.com>
2020-01-27 19:11:14 -0500

    Added api handler via axios within the utility directory. This file queries the api json data and allows the data to be saved to the mongo database. After this point the data is then able to be queried or deleted from the database by the user.

---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+    // Grab book json data from the Google books API
+    getBooks: function(q) {
+        return axios.get("/api/google", { params: { q: "title:" + q} });
+    },
+    // Get all saved book data from mongo database
+    getSavedBooks: function() {
+        return axios.get("/api/books");
+    },
+    // Delete any saved book by the book id
+    deleteBook: function(id) {
+        return axios.delete("/api/books" + id);
+    },
+    // Saves books to the database
+    saveBook: function(bookData) {
+        return axios.post("/api/books", bookData);
+    }
+};
